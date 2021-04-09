c670677f0cf87deeddb6a76e50c492f42a7393a8
unknown <avbochkareva1488@mail.ru>
2021-04-09 21:50:13 +0300

    code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long coins = 2_000_000_000;
+        long addcoins = 500_000_000;
+        long balance = coins + addcoins;
+        System.out.println(balance );
+    }
+}
